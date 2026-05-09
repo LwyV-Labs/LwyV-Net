c3fc526a407f8bbbb7e21f43f7fe54f95ea5b9e3
Apple <60138144+lwy183178053@users.noreply.github.com>
2026-05-09 11:39:19 +0800

    [F]简化配置文件

--- config/config.go
+++ config/config.go
@@ -14,54 +14,6 @@ import (
 	"github.com/LwyV-Labs/LwyV-Net/secure"
 )
 
-// Config 总配置结构体（对应整个yaml文件）
-type Config struct {
-	Server ServerConfig `json:"server"`
-	Client ClientConfig `json:"client"`
-}
-
-type BaseConfig struct {
-	PrivateKey     string          `json:"privateKey"`
-	PeerPublicKeys []string        `json:"peerPublicKeys"`
-	Identity       secure.Identity `json:"-"`
-	PeerStatic     []byte          `json:"-"`
-	Proxy          bool            `json:"proxy"`
-}
-
-// ServerConfig 服务端配置
-type ServerConfig struct {
-	BaseConfig
-	MTU      int         `json:"mtu"`
-	Port     int    `json:"port"`
-	IfName   string `json:"ifName"`
-	EgressIf string `json:"egressIf"`
-	VDHCP    VDHCPConfig `json:"vdhcp"`
-}
-
-type ServerEndpoint struct {
-	Name      string `json:"name"`
-	ServerIP  string `json:"ip"`
-	PublicKey string `json:"publicKey"`
-	MTU       int    `json:"mtu"`
-}
-
-// ClientConfig 客户端配置
-type ClientConfig struct {
-	BaseConfig
-	IfName      string           `json:"ifName"`
-	Servers     []ServerEndpoint `json:"servers"`
-	MTU         int              `json:"-"`
-	SelectedIdx int              `json:"-"`
-}
-
-// VDHCPConfig 虚拟DHCP配置
-type VDHCPConfig struct {
-	StartIP    string `json:"startIP"`
-	EndIP      string `json:"endIP"`
-	SubnetMask string `json:"subnetMask"`
-	Gateway    string `json:"gateway"`
-}
-
 const (
 	serverConfigPath = "server.json"
 	clientConfigPath = "client.json"
